1e39a9e58742bbf7465f17d142a8ad41f5a688cb
Adam Smallcomb <atsmallcomb@gmail.com>
2018-05-18 13:37:37 -0400

    give child a name to rebind

--- js/master.js
+++ js/master.js
@@ -63,7 +63,7 @@ displayBuffer = Lockr.get('displayBuffer') || [];
 var __CHILD_WINDOW_HANDLE = null;
 $("#open-display-btn").on('click', function() {
     $("#open-display-btn").trigger('blur');
-	__CHILD_WINDOW_HANDLE = window.open('display.html', '_blank', 'width=700,height=500,left=200,top=100');
+	__CHILD_WINDOW_HANDLE = window.open('display.html', 'displayname', 'width=700,height=500,left=200,top=100');
 });
 
 /* End Initialize */
